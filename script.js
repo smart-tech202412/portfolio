8265fe44b90d2734f0e518aa8b8564e67888d4fd
smart-tech202412 <abdurrehman202412@gmail.com>
2025-10-01 00:55:29 +0500

    portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('navbar-toggle').addEventListener('click', () => {
+    const menu = document.getElementById('navbar-menu');
+    menu.classList.toggle('active');
+  });
+  
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portfolio is ready!");
+});
